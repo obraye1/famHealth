137f8d235cc89062d9ea5d999fb00866c0585e02
bryan <obraye05@gmail.com>
2023-04-23 06:44:28 +0100

    NavBar Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import './index.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="bg-gray-100 h-screen flex justify-center items-center">
-      <h1 className="text-5xl font-bold text-gray-800 ">Hello, Tailwind!</h1>
-    </div>
+    <>
+      <NavBar />
+    </>
   );
 }
 
